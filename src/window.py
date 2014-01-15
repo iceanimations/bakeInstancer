88fdc0d9a7d7e4537f3b6ea39d37a60ccbbbba90
Qurban Ali <qurban.ali@iceanimations.com>
2014-01-15 11:45:39 +0500

    reset button added to window.ui and correspoding slot to window.py

--- src/window.py
+++ src/window.py
@@ -2,6 +2,7 @@ import site
 site.addsitedir(r"R:/Python_Scripts")
 from PyQt4.QtGui import *
 import plugins.utilities as utils
+import bake_particle_instancer as bpi
 from PyQt4 import uic
 import os
 osp = os.path
@@ -21,30 +22,58 @@ class Window(Form, Base):
         self.bakeButton.clicked.connect(self.bake)
         self.refreshButton.clicked.connect(self.refresh)
         self.selectAllButton.clicked.connect(self.selectAll)
+        self.resetButton.clicked.connect(self.resetWindow())
         
         #variables
         self.instancers = []
         
     def switchSelectAll(self):
+        """checks the selectAllButton if users checks all
+        the instancers on window"""
         select = True
         for inst in self.instancers:
             if inst.isChecked():
                 pass
             else: select = False; break
         self.selectAllButton.setChecked(select)
-        
+
     def selectAll(self):
+        """checks all the instancers when the user checks
+        selectAllButton"""
         select = self.selectAllButton.isChecked()
         for inst in self.instancers:
             inst.setChecked(select)
-        
+
+    def clearInstancers(self):
+        for inst in self.instancers:
+            inst.deleteLater()
+        self.instancers[:] = []
+        self.selectAllButton.setChecked(False)
+
+    def resetWindow(self):
+        self.clearInstancers()
+        self.timeSliderButton.setChecked(True)
+        self.startBox.clear()
+        self.endBox.clear()
+        self.stepBox.setValue(1.00)
+        self.listInstancers()
+
     def listInstancers(self):
-        pass
+        """Lists instancers on window"""
+        instancers = bpi.instancers()
+        for inst in instancers:
+            name = inst.split(":")[-1]
+            btn = QCheckBox(inst, self)
+            btn.clicked.connect(self.switchSelectAll)
+            self.instancersLayout.addWidget(btn)
+            self.instancers.append(btn)
         
     def bake(self):
         startFrame = None
         endFrame = None
         step = None
         
+        
     def refresh(self):
-        pass
+        self.clearInstancers()
+        self.listInstancers()
